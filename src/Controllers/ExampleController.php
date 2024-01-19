38a02b7e3fefa25823b97e94df9aa4cf3212efc5
Luc-cpl <lucas.carvalhopl@outlook.com>
2024-01-18 22:13:16 -0300

    update orkestra version

--- src/Controllers/ExampleController.php
+++ src/Controllers/ExampleController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-use Orkestra\Services\Http\Controllers\BaseHtmlController;
+use Orkestra\Services\Http\Controllers\AbstractHtmlController;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-class ExampleController extends BaseHtmlController
+class ExampleController extends AbstractHtmlController
 {
 	public function __invoke(ServerRequestInterface $request, array $args): ResponseInterface
 	{
