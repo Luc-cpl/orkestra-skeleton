e2d36be8d5207bb89b0dfc79a893ff1143f4ae23
Luc-cpl <lucas.carvalhopl@outlook.com>
2024-05-03 00:49:11 -0300

    update to orkestra v1.0.0

--- config/app.php
+++ config/app.php
@@ -1,12 +1,8 @@
 <?php
 
 return [
-	'env'         => 'development',
-	'root'        => __DIR__ . '/..',
-	'routes'      => __DIR__ . '/routes.php',
-	'public_path' => __DIR__ . '/../public',
-	'public_url'  => '/',
-	'slug'        => 'app',
-	'api_base'    => 'app',
-	'commands'    => [],
+	'env'    => 'development',
+	'root'   => __DIR__ . '/..',
+	'routes' => __DIR__ . '/routes.php',
+	'slug'   => 'app',
 ];
