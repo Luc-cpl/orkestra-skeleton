097823ae200f92d154a5ea0b208e922df38db674
Lucas Carvalho <lucas.carvalhopl@outlook.com>
2025-04-06 16:30:42 -0300

    chore: bump version to 1.1.1 and add EncryptionServiceProvider to providers

--- templates/vanilla/config/providers.php
+++ templates/vanilla/config/providers.php
@@ -1,11 +1,13 @@
 <?php
 
+use Orkestra\Providers\EncryptionServiceProvider;
 use Orkestra\Providers\CommandsProvider;
 use Orkestra\Providers\HooksProvider;
 use Orkestra\Providers\HttpProvider;
 use Orkestra\Providers\ViewProvider;
 
 return [
+	EncryptionServiceProvider::class,
 	CommandsProvider::class,
 	HooksProvider::class,
 	HttpProvider::class,
